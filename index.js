1f8afee887148a7c8441014ffb4c239487be4c5d
vibhu mahendru <vibhumahendru@gmail.com>
2018-09-13 12:41:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,13 +20,15 @@ function logWhisper(string) {
 //var lowercase = "hello!"
 //var uppercase = "HELLO!"
 function sayHiToGrandma(string){
-  if(string.toLowerCase()===string) {
-   return ('I can\'t hear you!')}
-   else if (string.toUpperCase()===string){
-     return('YES INDEED!')
-   }
-   else if (string==="I love you, Grandma."){
-     return ('I love you, too.')
-   }
+ if (string.toLowerCase()=== string){
+   return "I can't hear you!"
+ }
+ if (string.toUpperCase() === string){
+   return "YES INDEED!"
+ }
+ if (string === "I love you, Grandma."){
+   return "I love you, too."
+   
+ }
 
 }
